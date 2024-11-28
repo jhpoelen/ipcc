3d0cce634d66e67160a53d4a4a4070c80cd02540
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-11-28 10:40:14 -0600

    add tracking workflow

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Configuration to Track Reports of Intergovernmental Panel on Climate Change (IPCC)
+#
+
+preston track\
+ --algo md5\
+ https://www.ipcc.ch/report/ar6/wg1/downloads/report/IPCC_AR6_WGI_FullReport.pdf\
+ https://www.ipcc.ch/report/ar6/wg2/downloads/report/IPCC_AR6_WGII_FullReport.pdf\
+ https://www.ipcc.ch/report/ar6/wg3/downloads/report/IPCC_AR6_WGIII_FullReport.pdf

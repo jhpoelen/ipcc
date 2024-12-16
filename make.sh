a68a71ce25398e29cc8f054e7d9a946d49a1d7e4
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-12-16 11:41:25 -0600

    add synthesis reports as discussed with @DanielMietchen in context of https://www.wikidata.org/wiki/Q117820486

--- make.sh
+++ make.sh
@@ -7,4 +7,9 @@ preston track\
  --algo md5\
  https://www.ipcc.ch/report/ar6/wg1/downloads/report/IPCC_AR6_WGI_FullReport.pdf\
  https://www.ipcc.ch/report/ar6/wg2/downloads/report/IPCC_AR6_WGII_FullReport.pdf\
- https://www.ipcc.ch/report/ar6/wg3/downloads/report/IPCC_AR6_WGIII_FullReport.pdf
+ https://www.ipcc.ch/report/ar6/wg3/downloads/report/IPCC_AR6_WGIII_FullReport.pdf\
+ https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_SPM.pdf\
+ https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_LongerReport.pdf\
+ https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_FullVolume.pdf\
+ https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_AnnexesIndex.pdf\
+ https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_Errata.pdf
